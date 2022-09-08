38c161fe537066a74010024e3164bebd0494a29d
hari1538radha <hari.r2020ece@sece.ac.in>
2022-09-08 13:39:43 +0530

    create slice is been created and pushed

--- client/game-of-thrones/src/redux/DataSlice/CharSLice.js
+++ client/game-of-thrones/src/redux/DataSlice/CharSLice.js
@@ -1,4 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice ,createAsyncThunk } from "@reduxjs/toolkit";
 
-import { createAsyncThunk } from '@reduxjs/toolkit';
+export const getCharData = createAsyncThunk("Getdata", async ()=>
+{
+    return fetch("https://thronesapi.com/api/v2/Characters").then((response)=>
+    {
+         response.json()
+       
+    });
 
+}
+)
+
+export const CharSlice = createSlice(
+    {
+        name:"data",
+        initialState:
+        {
+            data: [],
+            loading:false
+        },
+        extraReducers:
+        {
+            [getCharData.pending]:(state,action)=>
+            {
+                state.loading =true;
+            },
+            [getCharData.fulfilled]:(state,action)=>
+            {
+                state.loading =false;
+                state.data = action.payload;
+            },
+            [getCharData.rejected]:(state,action)=>
+            {
+                state.loading =false;
+            }
+        }
+    }
+)
+export default CharSlice.reducer;
